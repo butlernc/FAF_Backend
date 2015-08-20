2a0d916887ef04b3ba04978b5571c0e6bc4334cd
Noah <butlernc@uwec.edu>
2015-08-20 10:43:44 -0700

    initial commit

---
+++ FAF_Backend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for FAF_Backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FAF_Backend.settings")
+
+application = get_wsgi_application()
